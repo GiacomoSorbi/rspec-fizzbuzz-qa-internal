462b303d85317c870bf33e9c127cd590f70067e6
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-28 12:41:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,3 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(n) n%15==0 ? "FizzBuzz" ? n%3==0 ? "Fizz" : n%5==0 ? "Buzz" : nil end
+def fizzbuzz(n) n%15==0 ? "FizzBuzz" : n%3==0 ? "Fizz" : n%5==0 ? "Buzz" : nil end
